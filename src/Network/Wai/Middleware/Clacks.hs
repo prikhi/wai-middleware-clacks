6a3e9ccdf8b1b3f038ac43c32e5616ce92d4a886
Pavan Rikhi <pavan.rikhi@gmail.com>
2020-03-13 04:30:30 -0400

    Add Initial Implementation
    Add a Network.Wai.Middleware.Clacks module containing a Clacks
    configuration type, sample configuration for GNU Terry Pratchett, & a
    clacks Wai middleware.

---
+++ src/Network/Wai/Middleware/Clacks.hs
@@ -0,0 +1,58 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Network.Wai.Middleware.Clacks
+    ( clacks
+    , Clacks(..)
+    , gnuTerryPratchett
+    )
+where
+
+import           Data.List.NonEmpty.Compat      ( NonEmpty(..)
+                                                , toList
+                                                )
+import           Data.Text.Encoding             ( encodeUtf8 )
+import           Network.HTTP.Types.Header      ( ResponseHeaders
+                                                , HeaderName
+                                                )
+import           Network.Wai                    ( Middleware
+                                                , modifyResponse
+                                                , mapResponseHeaders
+                                                )
+
+import qualified Data.ByteString               as BS
+import qualified Data.CaseInsensitive          as CI
+import qualified Data.List                     as L
+import qualified Data.Text                     as T
+
+
+-- | A "Network.Wai" 'Middleware' that adds a @X-Clacks-Overhead@ header containing
+-- the messages in the 'Clacks' configuration.
+clacks :: Clacks -> Middleware
+clacks settings = modifyResponse $ mapResponseHeaders addHeader
+  where
+    headerContents :: BS.ByteString
+    headerContents =
+        encodeUtf8 . T.intercalate "," . toList $ clacksMessages settings
+    addHeader :: ResponseHeaders -> ResponseHeaders
+    addHeader hs = case L.find ((== headerName) . fst) hs of
+        Nothing      -> (headerName, headerContents) : hs
+        Just (_, "") -> (headerName, headerContents) : hs
+        Just (name, contents) ->
+            (name, contents <> "," <> headerContents)
+                : filter ((/= headerName) . fst) hs
+
+-- | Configuration for the Clacks WAI Middleware.
+newtype Clacks =
+    Clacks
+        { clacksMessages :: NonEmpty T.Text
+        -- ^ The Clacks Messages to Include in the Header.
+        } deriving (Show, Read, Eq)
+
+
+-- | Sends a Clacks message of @GNU Terry Pratchett@.
+gnuTerryPratchett :: Clacks
+gnuTerryPratchett = Clacks $ "GNU Terry Pratchett" :| []
+
+
+-- | The name of the Clacks header.
+headerName :: HeaderName
+headerName = CI.mk "X-Clacks-Overhead"
